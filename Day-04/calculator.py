ab7e05a1e18e4e56372c1f91c8d0a44858080d39
satyanarayanabande <121191144+satyanarayanabande@users.noreply.github.com>
2024-11-30 15:28:51 +0530

    Create calculator.py

---
+++ Day-04/calculator.py
@@ -0,0 +1,17 @@
+num1 = 10
+num2 = 5
+
+addition = num1 + num2
+print(addition)
+
+sub = num1 - num2
+print(sub)
+
+mul = num1 * num2
+print(mul)
+
+divide = num1 / num2
+print(divide)
+
+mod = num1 % num2
+print(mod)
